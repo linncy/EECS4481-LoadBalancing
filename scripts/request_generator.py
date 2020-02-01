ded7ef886846a2bca4a5322b3383f351567d273a
Changyuan Lin <linchangyuan1996@gmail.com>
2020-02-01 02:51:25 -0500

    Add webapp and scripts

---
+++ scripts/request_generator.py
@@ -0,0 +1,57 @@
+"""
+HTTP Request Generator.  It simulates a number of concurrent users sending requests to the web server.
+"""
+
+import requests
+import time
+import threading
+import sys
+
+if len(sys.argv) < 4:
+    print('To few arguments; you need to specify 3 arguments.')
+    print('Default values will be used for server_ip, number of users and interval.\n')
+    server_ip = '127.0.0.1'  # ip address of the web server. Default value is localhost
+    no_users = 1  # number of concurrent users sending request to the web server
+    interval = 1  # interval in seconds between consequent requests
+else:
+    print('Default values have been overwritten.')
+    server_ip = sys.argv[1]
+    no_users = int(sys.argv[2])
+    interval = float(sys.argv[3])
+
+
+class HTTPThread(threading.Thread):
+    def __init__(self, name, counter):
+        threading.Thread.__init__(self)
+        self.threadID = counter
+        self.name = name
+        self.counter = counter
+
+    def run(self):
+        print("Starting " + self.name + str(self.counter))
+        send_request(self.name + str(self.counter))
+
+
+def send_request(user):
+    while True:
+        start_time = time.time()
+        try:
+            r = requests.get('http://' + server_ip + ':8000/')
+            data = r.json()
+        except requests.exceptions.RequestException as e:
+            print('e')
+        end_time = time.time()
+        time.sleep(interval)
+        print("Response Time for {} = {}; Response JSON: {}".format(user, end_time - start_time, data))
+
+
+if __name__ == "__main__":
+    threads = []
+    for i in range(no_users):
+        threads.append(HTTPThread("User", i))
+
+    for i in range(no_users):
+        threads[i].start()
+
+    for i in range(no_users):
+        threads[i].join()
---
+++ webapp/webapp.py
@@ -0,0 +1,44 @@
+"""
+a simple Flask web application with a compute-intensive workload;
+return the number of requests it has received and the amount of time it took to solve the compute-intensive workload
+difficulty can be changed by defining the environmental variable FACTORIAL, default is 20000
+"""
+
+from flask import Flask
+from redis import Redis
+import time
+import json
+import os
+
+app = Flask(__name__)
+redis = Redis(host='redis', port=6379)
+try:
+    difficulty = int(os.environ['FACTORIAL'])
+except:
+    difficulty = 20000
+
+try:
+    hostname = os.environ['HOSTNAME']
+except:
+    hostname = 'NA'
+
+def factorials(n = difficulty):
+    result = 1
+    start_time = time.time()
+    for i in range(1, n+1):
+        result *= i
+    end_time = time.time()
+    compute_time = end_time - start_time
+    return compute_time
+
+@app.route('/')
+def index():
+    request_timestamp = time.time()
+    count = redis.incr('requests', amount=1)
+    compute_time = factorials()
+    redis.lpush('log', json.dumps([request_timestamp, compute_time]))
+    return json.dumps({'Request_Count': count, 'Compute_Time': compute_time, 'Container_Name':hostname})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
