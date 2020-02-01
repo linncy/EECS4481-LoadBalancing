239faf55d3644d93f96aa00039002ae0ce8c8050
Changyuan Lin <linchangyuan1996@gmail.com>
2020-02-01 04:01:35 -0500

    Exception handling

--- scripts/request_generator.py
+++ scripts/request_generator.py
@@ -2,22 +2,26 @@
 HTTP Request Generator.  It simulates a number of concurrent users sending requests to the web server.
 """
 
+from redis import Redis
 import requests
+import json
 import time
 import threading
 import sys
 
 if len(sys.argv) < 4:
     print('To few arguments; you need to specify 3 arguments.')
-    print('Default values will be used for server_ip, number of users and interval.\n')
+    print('Default values will be used for server_ip, redis_ip, number of users and interval.\n')
     server_ip = '127.0.0.1'  # ip address of the web server. Default value is localhost
+    redis_ip = '172.18.0.2'
     no_users = 1  # number of concurrent users sending request to the web server
     interval = 1  # interval in seconds between consequent requests
 else:
     print('Default values have been overwritten.')
     server_ip = sys.argv[1]
-    no_users = int(sys.argv[2])
-    interval = float(sys.argv[3])
+    redis_ip = sys.argv[2]
+    no_users = int(sys.argv[3])
+    interval = float(sys.argv[4])
 
 
 class HTTPThread(threading.Thread):
@@ -36,16 +40,21 @@ def send_request(user):
     while True:
         start_time = time.time()
         try:
-            r = requests.get('http://' + server_ip + ':8000/')
+            r = requests.get('http://' + server_ip + ':8000/', timeout=60)
             data = r.json()
         except requests.exceptions.RequestException as e:
             print(e)
+            redis.lpush('errorlog', json.dumps([start_time, str(e)]))
+            continue
         end_time = time.time()
+        response_time = end_time - start_time
         time.sleep(interval)
-        print("Response Time for {} = {}; Response JSON: {}".format(user, end_time - start_time, data))
+        redis.lpush('log', json.dumps([start_time, data['Request_Count'], response_time, data['Compute_Time'], data['Container_Name']]))
+        print("Response Time for {} = {}; Response JSON: {}".format(user, response_time, data))
 
 
 if __name__ == "__main__":
+    redis = Redis(host=redis_ip, port=6379)
     threads = []
     for i in range(no_users):
         threads.append(HTTPThread("User", i))
